a2f43ef5e225df2c11fde54782f0323cdb2f0588
ichrak24 <guesmiichrak3@gmail.com>
2020-10-20 16:36:38 +0200

    checkpoint-state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Img from "./image.jpg";
+import {Card} from "react-bootstrap";
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+  
+      person: {
+        fullName: "Ichrak Guesmi",
+        bio: "Beginner FullStackjs Developer",
+        imgSrc: { Img },
+        profession: "Web Developer",
+        show: false,
+       
+      },
+    };
+  }
+
+   
+
+  componentDidMount(){
+   
+    setInterval(() => {
+        this.setState({count:this.state.count+1})
+    }, 1000);
 }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+
+
+
+
+    handleClick=() =>{
+      this.setState({
+        count: 0,
+        person:{...this.state.person , show:!this.state.person.show},
+      });
+    };
+   
+  
+    toggledButon = () => {
+      return (
+        <div className="btnToggled">
+          {this.state.person.show ? (
+          <div>
+             <p>{this.state.count} seconds since componentDidMount</p>
+             <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src={Img} />
+  <Card.Body>
+    <Card.Title>{this.state.person.fullName}</Card.Title>
+    <Card.Text>
+   {this.state.person.profession}
+    </Card.Text>
+    <Card.Title>{this.state.person.bio}</Card.Title>
+  </Card.Body>
+</Card>
+          </div>
+        ) : (
+          <p></p>
+        )}
+      </div>
+    );
+  };
+  render() {
+    return (
+      <div className="App">
+        <button
+          onClick={(e) => {
+            this.handleClick();
+          }}
+          type="button"
+          className="btn btn-primary"
+        >
+          Afficher Profil{" "}
+        </button>
+        {this.toggledButon()}
+      </div>
+    );
+  }
+}
+
+App.propTypes = {};
 
 export default App;
